83fb46c8eed023806bcd5ee318fb3f0eeb483f15
rakeshborole007 <46221705+rakeshborole007@users.noreply.github.com>
2020-02-24 12:45:41 +0530

    Delete main.dart

--- main.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  String s1 = '';
-  String s2;
-  String s3 = 'asd';
-  print('s1.isEmpty:: ${s1?.isEmpty ?? true}');
-  print('s2.isEmpty:: ${s2?.isEmpty ?? true}');
-  print('s3.isEmpty:: ${s3?.isEmpty ?? true}');
-}
