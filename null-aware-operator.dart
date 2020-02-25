b4346426360b337915f1d83ac509565ebd98026a
rakeshborole007 <46221705+rakeshborole007@users.noreply.github.com>
2020-02-25 12:58:51 +0530

    Update null-aware-operator.dart

--- null-aware-operator.dart
+++ null-aware-operator.dart
@@ -13,4 +13,32 @@ void main() {
   print('s1.isNotEmpty:: ${s1?.contains('a') ?? false}');
   print('s2.isNotEmpty:: ${s2?.contains('a') ?? false}');
   print('s3.isNotEmpty:: ${s3?.contains('a') ?? false}');
+
+  print('------------------------------------');
+
+  Function nullableFunctionInstance1 = (var1) {
+    return var1;
+  };
+  Function nullableFunctionInstance2;
+  print(
+      'nullableFunctionInstance1?.call("var1"):: ${nullableFunctionInstance1?.call("var1")}');
+  print(
+      'nullableFunctionInstance2?.call("var2"):: ${nullableFunctionInstance2?.call("var2")}');
+
+  print('------------------------------------');
+
+  shorterTheIfCondition();
+}
+
+shorterTheIfCondition() {
+  Map routeinfo = {"no_route": ""};
+  if (routeinfo["no_route"] == "" ||
+      routeinfo["no_route"] == null ||
+      routeinfo["no_route"] == false ||
+      routeinfo["no_route"] == 0) {
+    print('Inside longer version');
+  }
+  if (["", null, false, 0].contains(routeinfo["no_route"])) {
+    print('Inside shorter version');
+  }
 }
