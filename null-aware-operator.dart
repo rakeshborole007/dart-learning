64d4a985ca48a534febf3a6ee64944037e2347ea
rakeshborole007 <46221705+rakeshborole007@users.noreply.github.com>
2020-02-25 12:41:01 +0530

    Update null-aware-operator.dart

--- null-aware-operator.dart
+++ null-aware-operator.dart
@@ -5,4 +5,12 @@ void main() {
   print('s1.isEmpty:: ${s1?.isEmpty ?? true}');
   print('s2.isEmpty:: ${s2?.isEmpty ?? true}');
   print('s3.isEmpty:: ${s3?.isEmpty ?? true}');
+  print('------------------------------------');
+  print('s1.isNotEmpty:: ${s1?.isNotEmpty ?? false}');
+  print('s2.isNotEmpty:: ${s2?.isNotEmpty ?? false}');
+  print('s3.isNotEmpty:: ${s3?.isNotEmpty ?? false}');
+  print('------------------------------------');
+  print('s1.isNotEmpty:: ${s1?.contains('a') ?? false}');
+  print('s2.isNotEmpty:: ${s2?.contains('a') ?? false}');
+  print('s3.isNotEmpty:: ${s3?.contains('a') ?? false}');
 }
